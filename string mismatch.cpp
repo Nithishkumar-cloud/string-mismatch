33949955c937cfcea96ee16fcf10f99e18d9bdf7
nithish <84829934+Nithishkumar-cloud@users.noreply.github.com>
2022-03-26 13:28:49 +0530

    Add files via upload

---
+++ string mismatch.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	string s="nithish";
+	string d="njghdsfc";
+	int f=s.length();
+	for(int i=0;i<f;i++){
+		if(s[i]-0!=d[i]-0){
+			cout<<s[i]<<d[i]<<endl;
+		}
+	}
+}
